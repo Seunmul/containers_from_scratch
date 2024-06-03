342a88a40031a45bd9930fd9e5d76dbdc97499b0
GeonhaPark <geonhab504@gmail.com>
2024-06-03 17:28:00 +0900

    update readme.md

--- README.md
+++ README.md
@@ -8,6 +8,37 @@ gcc -o escape_chroot escape_chroot.c -static
 gcc -o cpu_test cpu_test.c -static
 ```
 
-```go
+```bash
+$ su ## root 권한으로 진행 su가 안되면 sudo 사용하기
+$ cd /
+$ mkdir /container_tmp
+$ cd container_tmp
+$ pwd
+# /container_tmp
+
+$ tree -L 1 .
+container_tmp
+├── create_container.sh
+├── entrypoint.sh
+├── main.go
+├── myroot.tar
+
+# 위와 같은 경로가 되게끔 이 되게끔 파일 복사 및 다운로드
+# 이후 myroot.tar 압축ㅎ해제
+tar -xvf myroot.tar
+
+$ tree -L 1 .
+.
+├── create_container.sh
+├── entrypoint.sh
+├── main.go
+├── myroot
+└── myroot.tar
+
+#이후 bash script 실행
+$ create_container.sh
+```
+
+```bash
 go run main.go run /bin/bash
 ```
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,23 +2,24 @@ echo "Running in an isolated environment with limited resources..."
 
 # ls
 echo "Setting up namespaces"
-# hostname container
-ip addr show
+hostname container
+# ip addr show
 
+# Setting up cgroups
 echo "Setting up cgroups\n"
-# cgroup pids디렉토리 생성
+## cgroup pids: create new cgroup below pids hierarchy
 containers_from_scratch_pids="/sys/fs/cgroup/pids/containers_from_scratch"
 mkdir -m 0755 -p "$containers_from_scratch_pids"
 
-# pids.max 파일에 최대 PID 수 제한 설정
+## pids.max: set the maximum number of processes
 echo "5" >"$containers_from_scratch_pids/pids.max"
 chmod 0700 "$containers_from_scratch_pids/pids.max"
 
-# notify_on_release 파일에 클린업 설정
+## notify_on_release: Cleanup the cgroup when it is empty
 echo "1" >"$containers_from_scratch_pids/notify_on_release"
 chmod 0700 "$containers_from_scratch_pids/notify_on_release"
 
-# add current process to the cgroup
+## add current process to the cgroup
 echo "$$" >"$containers_from_scratch_pids/cgroup.procs"
 
 ls -l "$containers_from_scratch_pids"
@@ -28,16 +29,20 @@ cat "$containers_from_scratch_pids/pids.max"
 
 # Set up the container root
 echo "initializing container"
-mount --bind ./myroot ./new_root/
-mkdir -p new_root/put_old
-pivot_root new_root new_root/put_old
-cd /
-mount -t proc proc /proc
-umount -l /put_old
-rmdir /put_old
+
+mkdir -p ./new_root               ## create a new root directory
+mount --bind ./myroot ./new_root/ ## bind the myroot directory to the new root directory
+mkdir -p ./new_root/put_old       ## create a new directory for the old root
+
+pivot_root ./new_root ./new_root/put_old ## pivot_root to the new root directory
+
+cd /                     ## change the directory to the root directory
+mount -t proc proc /proc ## mount the proc filesystem
+umount -l /put_old       ## unmount the old root directory
+rmdir /put_old           ## remove the old root directory
 
 echo "executing chroot"
-exec chroot . /bin/bash
+exec chroot . /bin/bash ## execute the bash shell in the new root directory
 
 # cat /etc/os-release
 # kill -9 $$
